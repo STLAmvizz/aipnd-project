749f36b198f54182bab2ab0ee08cd69a9f183f39
Michael Vizzini <michael.vizzini@stellantis.com>
2023-07-31 13:32:54 -0400

    Made updates to train.py to meet project requirements

--- train.py
+++ train.py
@@ -23,13 +23,33 @@ if __name__ == "__main__":
     ########################################################################################################################
     parser = argparse.ArgumentParser()
     parser.add_argument('data_dir', help="Directory in which data is held")
+    parser.add_argument('model_choice', help="v --> VGG16; r --> Resnet50")
+    parser.add_argument('learning_rate', help="0.001")
+    parser.add_argument('training_epochs', help="10")
+    parser.add_argument('gpu', help="y")
     parser.add_argument('model_dir', help="Directory in which the model will be saved")
     parser.add_argument('checkpoint_dir', help="Directory in which the checkpoint will be saved")
     args = parser.parse_args()
     ########################################################################################################################
 
-    # Instantiate the Model
+    # Test all of the inputs and their types
     ########################################################################################################################
+    # print("model choice: {}".format(args.model_choice))
+    # print("type model choice: {}".format(type(args.model_choice)))
+    # print("learning rate: {}".format(float(args.learning_rate)))
+    # print("type learning rate: {}".format(type(float(args.learning_rate))))
+    # print("training epochs: {}".format(int(args.training_epochs)))
+    # print("type training epochs: {}".format(type(int(args.training_epochs))))
+    # print("gpu: {}".format(args.gpu))
+    # print("type gpu: {}".format(type(args.gpu)))
+
+    # quit()
+    ########################################################################################################################
+
+    # Instantiate the Model(s)
+    ########################################################################################################################
+    lr = float(args.learning_rate)
+    
     # Set up the original VGG16 Model
     # Use the default weights for everything at first
     weightsvgg16 = models.VGG16_Weights.DEFAULT
@@ -57,24 +77,59 @@ if __name__ == "__main__":
     vgg16.classifier = classifier
     vgg16.name = 'vgg16'
 
-    optimizervgg16 = optim.Adam(vgg16.classifier.parameters(), lr=0.001)
+    optimizervgg16 = optim.Adam(vgg16.classifier.parameters(), lr=lr)
+
+    # Set up the original Resnet50 Model
+    # Use the default weights for everything at first
+    weightsrn50 = models.ResNet50_Weights.DEFAULT
+    resnet50 = models.resnet50(weights=weightsrn50)
+
+    # Freeze Parameters so we don't backprop through them
+    for param in resnet50.parameters():
+        param.requires_grad = False
+
+    fc = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(2048, 500)),
+        ('relu1', nn.ReLU()),
+        ('dropout1', nn.Dropout(0.2)),
+        ('fc2', nn.Linear(500, 255)),
+        ('relu2', nn.ReLU()),
+        ('dropout2', nn.Dropout(0.2)),
+        ('fc3', nn.Linear(255,102)),
+        ('output1', nn.LogSoftmax(dim=1))
+    ]))
+
+    resnet50.fc = fc
+    resnet50.name = 'resnet50'
+
+    optimizerresnet50 = optim.Adam(resnet50.fc.parameters(), lr=lr)
+
+    # Let the user choose which of the models they would like to train
+    if args.model_choice == 'v':
+        model = vgg16
+        optimizer = optimizervgg16
+    elif args.model_choice == 'r':
+        model = resnet50
+        optimizer = optimizerresnet50
     ########################################################################################################################
 
     # Train the instantiated model
     ########################################################################################################################
     path = args.model_dir
-    model = vgg16
     dataloaders_train = helper.transform_normalize_load_batch(args.data_dir)[0]
     dataloaders_valid = helper.transform_normalize_load_batch(args.data_dir)[1]
-    epochs = 10
+    epochs = int(args.training_epochs)
     print_every = 10
 
     # Optimizer & criterion
-    optimizer = optimizervgg16
     criterion = nn.NLLLoss()
 
     # Move the model over to the appropriate device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if args.gpu == 'y':
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        device = 'cpu'
+    
     model.to(device)
 
     # Some initalizations required for training/validating
@@ -165,7 +220,7 @@ if __name__ == "__main__":
     # Test the network post training
     ########################################################################################################################
     state_dict = torch.load(args.model_dir)
-    model = vgg16
+    model = model
     model.load_state_dict(state_dict)
     model.eval()
     model.to(device)
